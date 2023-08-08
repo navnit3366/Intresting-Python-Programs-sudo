b84efb55dce9cd0876d03db8f9c5e6d816940e34
Navnit P <navnit3366@gmail.com>
2023-08-08 10:59:53 +0530

    Deploying Intresting-Python-Programs-sudo

---
+++ Fake Information Generator.py
@@ -0,0 +1,7 @@
+#Python Fake Information Generator
+from faker import Faker
+fake = Faker()
+print(fake.name())
+print(fake.email())
+print(fake.country())
+print(fake.profile())                                                        
